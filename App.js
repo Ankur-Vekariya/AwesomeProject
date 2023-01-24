c3612d7a9d3eac02e41784471261cf2daeb62374
ankur12v <65348445+ankur12v@users.noreply.github.com>
2023-01-24 22:40:50 +0530

    React native app started

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ToastAndroid,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const showToast = () => {
+    console.log("Toast clicked...");
+    ToastAndroid.show(
+      "You clicked this toast!",
+      ToastAndroid.LONG,
+      ToastAndroid.TOP
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello app user</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={() => showToast()}>
+        <Text style={{ color: "#fff" }}>Press me!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +30,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    colors: "#304ffe",
+    fontSize: 20,
+    padding: 20,
+  },
+  button: {
+    backgroundColor: "#29b6f6",
+    borderRadius: 10,
+    width: 150,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
